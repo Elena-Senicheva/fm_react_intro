85f79febed6c8ede3fd755536fe2bf8a960aefc0
Elena-Senicheva <79080970+Elena-Senicheva@users.noreply.github.com>
2021-05-20 22:51:09 +0300

    add counter

--- index.js
+++ index.js
@@ -1,45 +1,33 @@
 const container = document.getElementById("root");
 
-/*class Heading extends React.Component {
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+    };
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
+  }
+  increment() {
+    const {counter} = this.state;
+    this.setState({counter: counter + 1});
+  }
+  decrement() {
+    const {counter} = this.state;
+    if(counter >0){
+      this.setState({counter: counter - 1});
+    }
+    
+  }
   render() {
     return React.createElement(
-      "h1",
-      {
-        title: "Hello react!",
-        className: "heading",
-      },
-      "Hello React.js!"
+      React.Fragment,
+      null,
+      React.createElement("h1", null, this.state.counter),
+      React.createElement("button", { onClick: this.decrement }, "-"),
+      React.createElement("button", { onClick: this.increment }, "+")
     );
   }
 }
-const reactElement = React.createElement(Heading);
-
-ReactDOM.render(reactElement, container);*/
-
-class Heading extends React.Component {
-  render() {
-    console.log(this);
-    const {name, classNameForHeading, titleForHeading, children} = this.props;
-    return React.createElement(
-      "h1",
-      {
-        title: titleForHeading,
-        className: classNameForHeading,
-      },
-      `Hello ${name}!`,
-      ...children,
-    );
-  }
-}
-
-const reactElement = React.createElement(Heading, {
-  name: "React",
-  titleForHeading: 'Hello Everyone!',
-  classNameForHeading: "heading test",
-},
-"test",
-"lorem",
-"something",
-);
-
-ReactDOM.render(reactElement, container);
+ReactDOM.render(React.createElement(Counter), container);
